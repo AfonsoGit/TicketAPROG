e78d27969f3d7d9ccfc431559e5cf737ea89f7f7
axdias <45363520+axdias@users.noreply.github.com>
2019-01-02 19:55:54 +0000

    Add files via upload

--- ultissimo.c
+++ ultissimo.c
@@ -175,9 +175,9 @@ int atend_entrega()
 			if(balc == 1 || balc == 2 || balc == 3 || balc == 4)
 			{					
 				printf("Valor a pagar:");
-				scanf("%s", &entrega1[k].cond_ent);
+				scanf("%f", &entrega1[k].valor_pagar);
 				//Validar que o valor a pagar e superior a 0
-				if(entrega1[k].cond_ent < 0)
+				if(entrega1[k].valor_pagar < 0)
 				{
 					printf("Valor invalido");
 				}else
@@ -205,7 +205,7 @@ int atend_entrega()
 //5. Listar todos os tickets por data
 int listar_ticket()
 {
-	int l;
+	int l,m;
 	printf("Servico de reparacao\n\n");
 	l=0;
 	do
@@ -219,9 +219,19 @@ int listar_ticket()
 		getchar();
 		l++;
 	}while(l<i);
-
 	printf("Servico de entrega\n\n");
-	l=0;
+	m=0;
+	do
+	{
+		printf("Ticket numero %d\n",entrega1[m].numtick_ent);
+		printf("Data/hora de entrada: %d-%d-%d %d:%d\n", entrega1[m].dia_entrada_ent, entrega1[m].mes_entrada_ent, entrega1[m].ano_entrada_ent, entrega1[m].hora_entrada_ent, entrega1[m].min_entrada_ent);
+		printf("Data/hora de atendimento: %d-&d-%d %d:%d\n", entrega1[m].hora_atend_ent, entrega1[m].min_atend_ent);
+		printf("Balcao: %d\n", entrega1[m].balcao_ent);
+		printf("Tipo de avaria:%s\n", entrega1[m].valor_pagar);
+		printf("Observacoes:%s\n\n\n", entrega1[m].cond_ent);
+		getchar();
+		m++;
+	}while(m<j);
 }
 
 int mapa_quantidades()
@@ -231,18 +241,42 @@ int mapa_quantidades()
 	printf("Foram criados no dia de hoje %d tickets\n",quant);
 return quant;
 }
-/*int mapa_medias()
+int mapa_medias()
 {
-	int minutos, horas, seg, y, media_rep ;
+	int minutos,minutosr, horas,horasr, minutose, horase, y, media_rep,media_ent,tempoespera_ent, tempoespera_rep, w, total, totalmedia;
 	y = 0;
+	horasr = 0;
+	minutosr = 0;
+	tempoespera_rep = 0;
 	do{
-	minutos = reparacao1[y].min_atend_rep - reparacao1[y].min_entrada_rep;
 	horas = reparacao1[y].hora_atend_rep - reparacao1[y].hora_entrada_rep;
+	horasr = (horas * 60);
+	tempoespera_rep = tempoespera_rep + (horasr + reparacao1[y].min_atend_rep) - reparacao1[y].min_entrada_rep;
 	y++;
 }while(y<i);
-media_rep = 
+minutose = 0;
+w = 0;
+horas = 0;
+horase = 0;
+tempoespera_ent = 0;
+do{
+	horas = entrega1[w].hora_atend_ent - entrega1[w].hora_entrada_ent;
+	horase = (horas * 60);
+	tempoespera_ent = tempoespera_ent + (horase + entrega1[w].min_atend_ent) - entrega1[w].min_entrada_ent;
+	w++;
+}while(w<j);
+media_rep = tempoespera_rep / y;
+media_ent = tempoespera_ent / w;
+total = tempoespera_rep + tempoespera_ent;
+totalmedia = (media_rep + media_ent) / 2;
+printf("O total de minutos de espera dos tickets de reparacao foi de %d minutos\n",tempoespera_rep);
+printf("O total de minutos de espera dos tickets de entrega foi de %d minutos\n",tempoespera_ent);
+printf("O total de minutos de espera da loja foi de %d\n",total);
+printf("A média de minutos de espera dos tickets de reparacao foi de %d minutos\n",media_rep);
+printf("A média de minutos de espera dos tickets de entrega foi de %d minutos\n",media_ent);
+printf("A média de minutos de espera da loja foi de %d\n",totalmedia);
 }
-*/
+
 int mapa_balcoes()
 {
 
@@ -275,40 +309,40 @@ int mapa_balcoes()
 }while (n<j);
 n = 0;
 do {
-	if (entrega1[n].balcao_ent == 1)
+	if (reparacao1[n].balcao_rep == 1)
 	{balcao1++;
 	n++;
 	}else
-	{if (entrega1[n].balcao_ent == 2){
+	{if (reparacao1[n].balcao_rep == 2){
 	balcao2++;
 	n++;
 	}else
-	{if (entrega1[n].balcao_ent == 3)
+	{if (reparacao1[n].balcao_rep == 3)
 	{balcao3++;
 	n++;
 	}
 	else{
-	if (entrega1[n].balcao_ent == 4){
+	if (reparacao1[n].balcao_rep == 4){
 	balcao4++;
 	n++;}
 	}
 	}
 	}
-}while (n<j);
+}while (n<i);
 printf("Balcao 1 : %d\nBalcao 2 : %d\nBalcao 3 :%d\nBalcao 4 : %d\n",balcao1, balcao2, balcao3,balcao4);
 	
 	
 }
 int receitas(){
-	float receitas;
+	float receitas, media;
 	int t;
 	receitas = 0;
 	t=0;
-	do{
+	for(t=0;t<j;t++){
 		receitas = receitas + entrega1[t].valor_pagar;
-		t++;
-	}while (t<j);
-printf("O total de receitas foi de %d\n",receitas);
+	}
+	media = receitas / j;
+printf("O total de receitas foi de %f euros.\n e a media foi de %f",receitas, media);
 }
 int main()
 {
@@ -351,7 +385,7 @@ int main()
 		}
 		if(opcao == 7)
 		{
-			
+			mapa_medias();
 		}
 		if(opcao == 8)
 		{
@@ -368,7 +402,7 @@ int main()
 		system("pause");
 		system("cls");
 		printf("Software de Gestao de Tickets\n\n\n");
-		printf("1-Ticket Reparacao\n2-Ticket Entrega\n3-Atendimento Reparacao\n4-Atendimento de entrega\n5-Listar todos os tickets\n6-Mapas de quantidades\n7-Mapas de medias\n8-Mapas de balcoes\n9-Receitas\n10-Sair\n\nOpcao:");	
+		printf("1-Ticket Reparacao\n2-Ticket Entrega\n3-Atendimento Reparacao\n4-Atendimento de entrega\n5-Listar todos os tickets\n6-Mapas de quantidades\n7-Mapas de medias de espera\n8-Mapas de balcoes\n9-Receitas\n10-Sair\n\nOpcao:");	
 	}while(opcao!=9);
 return 0;		
 }
